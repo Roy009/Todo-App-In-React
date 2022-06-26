b2816efe13577bb39c76bbcda468450e6f10cd33
Rohit009 <63160366+Roy009@users.noreply.github.com>
2022-06-26 22:10:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useState } from 'react';
 
 function App() {
+  const [listItems, setTodoItems] = useState([]);
+  const [listText, setTodoText] = useState("");
+
+
+  const handleChange = ({ target }) => {
+    setTodoText(target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodoItems((items) => [...items, listText]);
+    setTodoText("");
+  };
+  const deleteTodo = (index) => {
+    listItems.splice(index, 1);
+    setTodoItems((items) => [...items, listText]);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TO DO</h1>
       </header>
+      <form onSubmit={handleSubmit}>
+        <label className="todolabel">What do you want to do today ?</label>
+        <br />
+        <input type="text" className="input" value={listText} onChange={(event) => {
+          handleChange(event);
+        }} />
+        <button className="button" type="submit">
+          Add
+        </button>
+      </form>
+      {listItems.map(
+        (todo, i) =>
+          todo !== "" && (
+            <>
+              <div className="todo">
+                <br />
+                <input key={i} type="checkbox" />
+                <label>{todo}</label>
+                <IconButton key={i} onClick={() => deleteTodo(i)}>
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            </>
+          )
+      )}
     </div>
   );
 }
